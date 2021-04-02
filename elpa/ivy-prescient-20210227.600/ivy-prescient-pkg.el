9c7dbca63fc26ffb9ccaaf38703c5bf8398fd231
brian greenfield <briang@cpan.org>
2021-04-02 13:49:22 +0100

    snapshot 2021-04-02 13:49:22

---
+++ elpa/ivy-prescient-20210227.600/ivy-prescient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ivy-prescient-20210227.600/ivy-prescient.el  -*- no-byte-compile: t -*-
+(define-package "ivy-prescient" "20210227.600" "prescient.el + Ivy" '((emacs "25.1") (prescient "5.1") (ivy "0.11.0")) :commit "8573df977eaceffc6607b7242ff8c0dab02aad65" :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :keywords '("extensions") :url "https://github.com/raxod502/prescient.el")
